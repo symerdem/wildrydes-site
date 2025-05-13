a56f0910109571e56555337841643d69d4bc84ed
symerdem <83474139+symerdem@users.noreply.github.com>
2025-05-13 14:24:25 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: us-east-1_ic72ouKvh  '', // us-east-1_ic72ouKvh
+        userPoolClientId: 45ntu5j78a1fvs6dvd7iibd8v '', // 45ntu5j78a1fvs6dvd7iibd8v
+        region: us-east-1 '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
