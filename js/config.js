4eff0c5dbad62244f870189f477cf56f84039529
symerdem <83474139+symerdem@users.noreply.github.com>
2025-05-13 14:32:38 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,9 @@
-window._config = {
-    cognito: {
-        userPoolId: us-east-1_ic72ouKvh  '', // us-east-1_ic72ouKvh
-        userPoolClientId: 45ntu5j78a1fvs6dvd7iibd8v '', // 45ntu5j78a1fvs6dvd7iibd8v
-        region: us-east-1 '' // e.g. us-east-1
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+js
+const config = {
+  ...
+  Auth: {
+    userPoolId: 'us-east-1_ic72ouKvh', // ← User Pool ID
+    userPoolWebClientId: '45ntu5j78a1fvs6dvd7iibd8v', // ← App Client ID
+    region: 'us-east-1', // ← kendi Region bilginiziyaz
+  }
 };
