e56a3a894b146d1594f597c48a0d6410c783a525
symerdem <83474139+symerdem@users.noreply.github.com>
2025-05-13 15:14:30 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,10 @@
-js
-const config = {
-  ...
-  Auth: {
-    userPoolId: 'us-east-1_WA8133eWT', // ← User Pool ID
-    userPoolWebClientId: '632ckejl9odjmt24p7s9ccpdl9', // ← App Client ID
-    region: 'us-east-1', // ← kendi Region bilginiziyaz
+window._config = {
+  cognito: {
+    userPoolId: 'us-east-1_WA8133eWT', // ← senin User Pool ID
+    userPoolClientId: '632ckejl9odjmt24p7s9ccpdl9', // ← senin App Client ID
+    region: 'us-east-1' // ← bulunduğun AWS bölgesi
+  },
+  api: {
+    invokeUrl: '' // ← API Gateway URL'in varsa buraya yazılacak
   }
 };
